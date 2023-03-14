70c3b1ffa9705c3f33d393c1fa316b739c575b9f
Suraj <ssingh.8721382@gmail.com>
2023-03-14 16:23:56 +0530

    string formating

---
+++ string formating.py
@@ -0,0 +1,7 @@
+def  print_formatted(n):
+    l=len(bin(n)[2:])
+    for i in range(1,n+1):
+        print(str(i).rjust(l),(oct(i)[2:]).rjust(l),(hex(i)[2:]).upper().rjust(l),(bin(i)[2:]).rjust(l))
+
+
+
